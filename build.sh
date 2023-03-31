f9511908adec13c7940e474d8522c3b3437a8803
Hidetoshi <kwkbhdts@gmail.com>
2023-03-31 18:41:52 +0900

    Add a debug build option to build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,23 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 pushd $SCRIPT_DIR
 
-cargo build --release
+while getopts d OPT
+do
+  case $OPT in
+    "d" ) DEBUG_FLAG=true ;;
+  esac
+done
+
 mkdir -p bin
-cp -f target/release/zabel.exe bin/zabel.exe
+
+if "${DEBUG_FLAG}"; then
+    cargo build
+    cp -f target/debug/zabel.exe bin/zabel.exe
+else
+    cargo build --release
+    cp -f target/release/zabel.exe bin/zabel.exe
+fi
+
 cp -rf resource bin/
 
 # Back to the saved directory.
