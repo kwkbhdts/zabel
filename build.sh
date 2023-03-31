e9c66f07b0569150c5fa5f67391800816daa1c91
Hidetoshi <kwkbhdts@gmail.com>
2023-03-31 15:05:21 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Move to the directory of this script.
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 pushd $SCRIPT_DIR
 
-cargo build --release
+cross build --target x86_64-pc-windows-gnu --release
 mkdir -p bin
-cp -f target/release/zabel.exe bin/zabel.exe
+cp -f target/x86_64-pc-windows-gnu/release/zabel.exe bin/zabel.exe
 cp -rf resource bin/
 
+# Back to the saved directory.
 popd
