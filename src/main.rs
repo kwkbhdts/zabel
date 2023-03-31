7708b920b35a3fe0ae02072cd3ed1e8233edde17
Hidetoshi <kwkbhdts@gmail.com>
2023-03-31 18:57:55 +0900

    Rename files

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::env;
 
 use crate::config::Config;
 
-mod debug_util;
-mod error_util;
+mod debug_utility;
+mod error_utility;
 mod config;
 mod bounding_box;
 
@@ -20,10 +20,10 @@ fn main() {
 }
 
 /// This function loads Config from a config file.
-fn load_config_file() -> error_util::Result<Config> {
+fn load_config_file() -> error_utility::Result<Config> {
     let exe_file_path = env::current_exe()?;
     let exe_dir_path = exe_file_path.parent()
-        .ok_or(error_util::create_error("Failed to get a parent path."))?;
+        .ok_or(error_utility::create_error("Failed to get a parent path."))?;
     let config_file_path = exe_dir_path.join(CONFIG_FILE_PATH);
     let config = Config::from_file(&config_file_path)?;
     Ok(config)
